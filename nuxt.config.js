f5bc047101c7d86061bc6249430c1212b35861f3
f4rr3ll90 <00mazahaka00@gmail.com>
2018-02-26 17:10:52 +0200

    sitemap added

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,22 @@
 const axios = require('axios')
-
+const routes = function() {
+  return axios({
+    url: "https://nuxtrest-2bb1.restdb.io/rest/posts",
+    method: "get",
+    headers: {
+      "content-type": "application/json",
+      "x-apikey": "5a91ed5116d5526228b426f0",
+      "cache-control": "no-cache"
+    }
+  })
+    .then((res) => {
+      var rts = []
+      res.data.forEach((d) => {
+        rts.push('/articles/' + d._id)
+      })
+      return rts
+    })
+}
 module.exports = {
   loading: {
     color: '#7A0B76',
@@ -16,9 +33,7 @@ module.exports = {
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  modules: [
-    'bootstrap-vue/nuxt'
-  ],
+
   css: [
     '~/node_modules/normalize.css/normalize.css',
     '~/assets/global.css'
@@ -28,25 +43,23 @@ module.exports = {
     // base: '/simple-blog-with-nuxt/'
   },
   generate: {
-    routes () {
-      return axios({
-        url: "https://nuxtrest-2bb1.restdb.io/rest/posts",
-        method: "get",
-        headers: {
-          "content-type": "application/json",
-          "x-apikey": "5a91ed5116d5526228b426f0",
-          "cache-control": "no-cache"
-        }
-      })
-        .then((res) => {
-          var rts = []
-          res.data.forEach((d) => {
-            rts.push('/articles/' + d._id)
-          })
-          return rts
-        })
-    }
+    routes: routes
   },
+  sitemap: {
+    path: '/sitemap.xml',
+    hostname: 'http://f4rr3ll.cc.ua',
+    cacheTime: 1000 * 60 * 15,
+    generate: true, // Enable me when using nuxt generate 
+    routes: routes
+  },
+  modules: [
+    'bootstrap-vue/nuxt',
+    ['@nuxtjs/sitemap', {
+      path: '/sitemap.xml',
+      generate: true,
+      routes: routes
+    }]
+  ],
   build: {
     vendor: [
       'axios'
